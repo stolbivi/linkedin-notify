22fbb97db94eff8fa723ecaaf3c85114d72fa05e
stolbivi <viktor.stolbin@gmail.com>
2022-12-10 22:16:59 -0500

    Init commit

---
+++ app/src/components/Main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+export const Main: React.FC<Props> = ({}) => {
+
+    return (
+        <div className="p-5 d-flex flex-column justify-content-center align-items-center">
+            <div>LinkedIn Notify</div>
+            <div>Extension to monitor activity on your LinkedIn profile</div>
+            <small>This extension was generated with Anodizer</small>
+        </div>
+    );
+};
---
+++ app/webpack.core.js
@@ -0,0 +1,116 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+const BABEL_OPTIONS = {
+  cacheDirectory: true,
+  cacheCompression: false,
+  envName: "development",
+};
+
+const initEntry = (dist, inputTs, outputJs) => {
+  return {
+    entry: inputTs,
+    output: {
+      path: path.resolve(__dirname, dist),
+      filename: outputJs,
+      publicPath: "/",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: BABEL_OPTIONS,
+            },
+            {
+              loader: "ts-loader",
+            },
+          ],
+        },
+        {
+          test: /\.js(x?)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: BABEL_OPTIONS,
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+      fallback: {
+      }
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        "process": {
+          env: {DEBUG: undefined},
+        }
+      })
+    ],
+    devtool: "cheap-module-source-map"
+  };
+};
+
+const addCSSModules = (object, outputCss) => {
+  object.module.rules.push({
+    test: /\.(s[ac]ss|css)$/i,
+    use: [
+      MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          importLoaders: 1,
+        },
+      },
+      "postcss-loader",
+      "sass-loader",
+    ],
+  });
+  object.plugins = object.plugins.concat([
+    new MiniCssExtractPlugin({
+      filename: outputCss,
+    }),
+  ].filter(Boolean));
+  return object;
+};
+
+const addHTMLModules = (object, template, outputHtml, outputCss) => {
+  object.module.rules.push({
+    test: /\.(s[ac]ss|css)$/i,
+    use: [
+      MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          importLoaders: 1,
+        },
+      },
+      "postcss-loader",
+      "sass-loader",
+    ],
+  });
+  object.plugins = object.plugins.concat([
+    new MiniCssExtractPlugin({
+      filename: outputCss,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, template),
+      inject: true,
+      filename: outputHtml,
+    }),
+  ].filter(Boolean));
+  return object;
+};
+
+module.exports = {
+  initEntry: initEntry,
+  addCSSModules: addCSSModules,
+  addHTMLModules: addHTMLModules,
+};
