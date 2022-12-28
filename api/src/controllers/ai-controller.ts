c5060483f66aaab7501a4d76dce9f532d76afbd5
stolbivi <viktor.stolbin@gmail.com>
2022-12-27 23:12:28 -0500

    AI implemented

--- api/src/controllers/ai-controller.ts
+++ api/src/controllers/ai-controller.ts
@@ -1,19 +1,43 @@
 import {Controller, Get, Query, Route} from "tsoa";
+import {Configuration, OpenAIApi} from "openai";
+
 
 @Route("/api")
 export class RecordsController extends Controller {
 
+    private openai;
+    private model = process.env.AI_MODEL;
+    private temperature = process.env.AI_TEMPERATURE;
+    private maxTokens = process.env.AI_MAX_TOKENS;
+
     constructor() {
         super();
+        this.openai = new OpenAIApi(new Configuration({
+            apiKey: process.env.AI_KEY,
+        }))
     }
 
-    @Get("ai")
-    public async check(@Query() request: string): Promise<any> {
-        console.log(`${new Date().toLocaleTimeString()} Running query`);
-        // console.error(error);
-        // this.setStatus(422);
-        // return {message: error};
-        return {response: "OK"};
+    @Get("completion")
+    public async completion(@Query() prompt: string,
+                            @Query() temperature?: number,
+                            @Query() maxTokens?: number
+    ): Promise<any> {
+        console.log(`${new Date().toLocaleTimeString()} Running completion query for:`, prompt, temperature, maxTokens);
+        try {
+            const completion = await this.openai.createCompletion({
+                model: this.model,
+                prompt,
+                temperature: Number(temperature ? temperature : this.temperature),
+                max_tokens: Number(maxTokens ? maxTokens : this.maxTokens),
+            });
+            return Promise.resolve({response: completion.data.choices});
+        } catch (error) {
+            const message = error.response
+                ? {data: error.response.data, status: error.response.status}
+                : {message: error.message};
+            this.setStatus(422);
+            return Promise.resolve(message);
+        }
     }
 
 }
