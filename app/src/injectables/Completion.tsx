4486006e6d21188ec06265b2c0e86e4bdf25c381
stolbivi <viktor.stolbin@gmail.com>
2023-01-02 15:53:00 -0500

    Added scrolling for post and fixed truncation issue

--- app/src/injectables/Completion.tsx
+++ app/src/injectables/Completion.tsx
@@ -6,6 +6,8 @@ import stylesheet from "./Completion.scss";
 
 type Props = {};
 
+const SCROLL_END = 100000; // just veri big number to cover
+
 export const Completion: React.FC<Props> = ({}) => {
 
     const messages = new Messages(MESSAGE_ID, VERBOSE);
@@ -14,19 +16,27 @@ export const Completion: React.FC<Props> = ({}) => {
     const [disabled, setDisabled] = useState(true);
     const [inProgress, setInProgress] = useState(false);
     const [editable, setEditable] = useState(null);
+    const [modal, setModal] = useState(null);
+    const [scrollable, setScrollable] = useState(null);
     const [text, setText] = useState("");
 
     useEffect(() => {
+        function listener(e: InputEvent) {
+            // @ts-ignore
+            const text = e.target.innerText.trim();
+            setDisabled(text.length === 0);
+            setText(text);
+        }
+
         const textDiv = document.getElementsByClassName("ql-editor");
+        const modalDiv = document.getElementsByClassName("artdeco-modal");
+        const scrollableDiv = document.getElementsByClassName("share-creation-state__content-scrollable");
         if (textDiv && textDiv[0]) {
             setEditable(textDiv[0]);
-            textDiv[0].addEventListener("keyup", (e: InputEvent) => {
-                // @ts-ignore
-                const text = e.target.innerText.trim();
-                setDisabled(text.length === 0);
-                setText(text);
-            }, false);
-
+            setModal(modalDiv[0]);
+            setScrollable(scrollableDiv[0]);
+            textDiv[0].addEventListener("keyup", listener, false);
+            textDiv[0].addEventListener("paste", listener, false);
         }
     }, []);
 
@@ -34,9 +44,14 @@ export const Completion: React.FC<Props> = ({}) => {
         if (position < text.length) {
             setTimeout(() => {
                 editable.innerText = text.substring(0, position);
+                modal.scrollTop = SCROLL_END;
+                scrollable.scrollTop = SCROLL_END;
                 simulateTyping(text, position + 3);
             }, 1);
         } else {
+            editable.innerText = text;
+            modal.scrollTop = SCROLL_END;
+            scrollable.scrollTop = SCROLL_END;
             setInProgress(false)
         }
     }
