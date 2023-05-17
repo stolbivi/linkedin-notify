d4cbd40ff3988d920e74c4c57075e9e0836710e8
viktorstolbin <viktor.stolbin@gmail.com>
2023-05-17 19:11:22 -0400

    Sentry integration for App and API

--- app/src/inpage.tsx
+++ app/src/inpage.tsx
@@ -13,32 +13,41 @@ import {LastViewedFactory} from "./injectables/LastViewed";
 import {TimeZoneFactory} from "./injectables/TimeZone";
 import {LnDashboardFactory} from "./injectables/LnDashboard";
 import {unmountComponent} from "./utils/InjectHelper";
-
+import * as Sentry from "@sentry/react";
 
 console.debug('LinkedIn Manager extension engaged');
+
+export const SENTRY_DISABLED = `${process.env.SENTRY_DISABLED}`;
+
+if (SENTRY_DISABLED === "false") {
+    console.log("Enabling Sentry reporting");
+    Sentry.init({
+        dsn: `${process.env.SENTRY_URL}`,
+    });
+}
 
 const dynamicUI = new DynamicUI();
 let initialLoad = true;
 let isWatchAutoFeature = false;
 
 const componentFactoryMap = {
-    Completion: { watch: true, element: CompletionFactory},
-    Salary: { watch: true, element: SalaryPillFactory},
-    Maps: { watch: true, element: MapsFactory},
-    AutoFeature: { watch: true, element: AutoFeatureFactory},
-    StagePill: { watch: true, element: StagePillFactory},
-    NotesAndCharts: { watch: true, element: NotesAndChartsFactory},
-    NotesManager: { watch: true, element: NotesManagerFactory},
-    LastViewed: { watch: true, element: LastViewedFactory},
-    TimeZone: { watch: true, element: TimeZoneFactory},
-    LnDashboard: { watch: true, element: LnDashboardFactory}
+    Completion: {watch: true, element: CompletionFactory},
+    Salary: {watch: true, element: SalaryPillFactory},
+    Maps: {watch: true, element: MapsFactory},
+    AutoFeature: {watch: true, element: AutoFeatureFactory},
+    StagePill: {watch: true, element: StagePillFactory},
+    NotesAndCharts: {watch: true, element: NotesAndChartsFactory},
+    NotesManager: {watch: true, element: NotesManagerFactory},
+    LastViewed: {watch: true, element: LastViewedFactory},
+    TimeZone: {watch: true, element: TimeZoneFactory},
+    LnDashboard: {watch: true, element: LnDashboardFactory}
 };
 const injectUI = () => {
     dynamicUI.watch(document, {
         subtree: true,
         childList: true,
         onAdd: (_node: Node) => {
-            if(initialLoad) {
+            if (initialLoad) {
                 CompletionFactory();
                 SalaryPillFactory();
                 MapsFactory();
@@ -61,7 +70,7 @@ const injectUI = () => {
 injectUI();
 
 // Send a message to the background script to inform it that the content script is ready
-chrome.runtime.sendMessage({ contentScriptReady: true }, () => {
+chrome.runtime.sendMessage({contentScriptReady: true}, () => {
     if (chrome.runtime.lastError) {
         console.error("Error sending message:", chrome.runtime.lastError.message);
     }
@@ -80,21 +89,21 @@ window.addEventListener("message", (event) => {
             htmlElement.setAttribute("data-theme", "light");
             htmlElement.classList.remove("theme--dark");
             htmlElement.classList.add("theme--light");
-            document.getElementById("ui-theme-dark").setAttribute("disabled","true");
+            document.getElementById("ui-theme-dark").setAttribute("disabled", "true");
         }
-    } else if(event.data.type === "modifyElements") {
+    } else if (event.data.type === "modifyElements") {
         initialLoad = event.data.initialLoad;
         const proFeatures = JSON.parse(sessionStorage.getItem("proFeatures"));
         proFeatures && Object.keys(proFeatures).length > 0 && Object.values(proFeatures).forEach(feature => {
-            if(feature.isChanged) {
-                if(!feature.isActive) {
-                    if("AutoFeature" === feature.id) {
+            if (feature.isChanged) {
+                if (!feature.isActive) {
+                    if ("AutoFeature" === feature.id) {
                         isWatchAutoFeature = false;
                     }
                     unmountComponent(feature.id);
                 } else {
-                    if(componentFactoryMap[feature.id]) {
-                        if("AutoFeature" === feature.id) {
+                    if (componentFactoryMap[feature.id]) {
+                        if ("AutoFeature" === feature.id) {
                             isWatchAutoFeature = true;
                         }
                         componentFactoryMap[feature.id].element();
